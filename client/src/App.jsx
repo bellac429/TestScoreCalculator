9fdba28c399503d4dd8d0e519b2627664596843b
bellac429 <isabella-conrad@uiowa.edu>
2025-02-05 11:28:00 -0600

    added graph function

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,9 +4,9 @@ import { LineChart, Line, XAxis, YAxis, Tooltip, CartesianGrid } from "recharts"
 import { InputLabel, MenuItem, FormControl, Select, FormHelperText, Button } from "@mui/material"
 
 function App() {
+    const [uploaded, setUploaded] = useState(false)
+
     const [file, setFile] = useState(null);
-    const [median, setMedian] = useState(null);
-    const [scores, setScores] = useState([]);
     const [testType, setTestType] = useState("Fastbridge");
     const [gradeLevel, setGradeLevel] = useState("K");
 
@@ -14,7 +14,12 @@ function App() {
     const handleTestTypeChange = (event) => setTestType(event.target.value);
     const handleGradeLevelChange = (event) => setGradeLevel(event.target.value);
 
+    const [median, setMedian] = useState(null);
+    const [scores, setScores] = useState([]);    
+
     const handleUpload = async () => {
+        setMedian(null)
+        setScores([])
         if (!file) return alert("Please select a file.");
 
         const formData = new FormData();
@@ -22,74 +27,31 @@ function App() {
         formData.append("testType", testType);
         formData.append("gradeLevel", gradeLevel);
 
+        console.log("form data: ")
+        // Display the key/value pairs
+        for (const pair of formData.entries()) {
+            console.log(pair[0], pair[1]);
+        }
+
         try {
-            const response = await axios.post("http://localhost:5000/upload", formData, {
+            const response = await axios.post("http://localhost:8000/upload", formData, {
                 headers: { "Content-Type": "multipart/form-data" },
             });
 
+            console.log("response", response)
+
             setMedian(response.data.median);
             setScores(response.data.scores || []);
+            setUploaded(true)
         } catch (error) {
             console.error("Error uploading file:", error);
         }
     };
 
-    return (
-        <div className="app">
-            <h1 className="hero">Test Score Analyzer</h1>
-
-
-            <div className="form">
-                <FormControl sx={{ m: 1, minWidth: 80 }} fullWidth>
-                    <InputLabel id="test-type-label">Test Type</InputLabel>
-                    <Select
-                        labelId="test-type-label"
-                        id="test-type"
-                        value={testType}
-                        label="Test Type"
-                        onChange={handleTestTypeChange}
-                        
-                    >
-                        <MenuItem value={"Fastbridge"}>Fastbridge</MenuItem>
-                        <MenuItem value={"DIBELS"}>DIBELS</MenuItem>
-                        <MenuItem value={"CBM"}>CBM</MenuItem>
-                    </Select>
-                    <FormHelperText>Select test type</FormHelperText>
-                </FormControl>
-
-                <FormControl sx={{ m: 1, minWidth: 80 }} fullWidth>
-                    <InputLabel id="grade-level-label">Grade Level</InputLabel>
-                    <Select
-                        labelId="grade-level-label"
-                        id="grade-level"
-                        value={gradeLevel}
-                        label="Grade Level"
-                        onChange={handleGradeLevelChange}
-                        
-                    >
-                        <MenuItem value={"K"}>K</MenuItem>
-                        <MenuItem value={"1"}>1</MenuItem>
-                        <MenuItem value={"2"}>2</MenuItem>
-                        <MenuItem value={"3"}>3</MenuItem>
-                        <MenuItem value={"4"}>4</MenuItem>
-                        <MenuItem value={"5"}>5</MenuItem>
-                    </Select>
-                    <FormHelperText>Select grade level</FormHelperText>
-                </FormControl>
-
-                <label > Upload CSV or EXCEL file
-                    <input type="file" onChange={handleFileChange} style={{ marginLeft: "1em"}}/>
-                </label>
-                
-                <button onClick={handleUpload}>Upload</button>
-            </div>
-            
-            
-
-            {/* Output Median and Graph*/}
-            {median !== null && <h2>Median: {median}</h2>}
-
-            {scores.length > 0 && (
+    if (uploaded === true) {
+        return (
+            <div className="app">
+                <h2>Median: {median}</h2>
                 <LineChart width={500} height={300} data={scores.map((score, index) => ({ index, score }))}>
                     <XAxis dataKey="index" />
                     <YAxis />
@@ -97,36 +59,63 @@ function App() {
                     <Tooltip />
                     <Line type="monotone" dataKey="score" stroke="#8884d8" />
                 </LineChart>
-            )}
-        </div>
-    );
+
+                <button onClick={() => setUploaded(false)}>Upload Again</button>
+            </div>
+        )
+
+    } else if (uploaded === false) {
+        return (
+            <div className="app">
+                <h1 className="hero">Test Score Analyzer</h1>
+    
+                <div className="form">
+                    <FormControl sx={{ m: 1, minWidth: 80 }} fullWidth>
+                        <InputLabel id="test-type-label">Test Type</InputLabel>
+                        <Select
+                            labelId="test-type-label"
+                            id="test-type"
+                            value={testType}
+                            label="Test Type"
+                            onChange={handleTestTypeChange}
+                        >
+                            <MenuItem value={"Fastbridge"}>Fastbridge</MenuItem>
+                            <MenuItem value={"DIBELS"}>DIBELS</MenuItem>
+                            <MenuItem value={"CBM"}>CBM</MenuItem>
+                        </Select>
+                        <FormHelperText>Select test type</FormHelperText>
+                    </FormControl>
+    
+                    <FormControl sx={{ m: 1, minWidth: 80 }} fullWidth>
+                        <InputLabel id="grade-level-label">Grade Level</InputLabel>
+                        <Select
+                            labelId="grade-level-label"
+                            id="grade-level"
+                            value={gradeLevel}
+                            label="Grade Level"
+                            onChange={handleGradeLevelChange}
+                            
+                        >
+                            <MenuItem value={"K"}>K</MenuItem>
+                            <MenuItem value={"1"}>1</MenuItem>
+                            <MenuItem value={"2"}>2</MenuItem>
+                            <MenuItem value={"3"}>3</MenuItem>
+                            <MenuItem value={"4"}>4</MenuItem>
+                            <MenuItem value={"5"}>5</MenuItem>
+                        </Select>
+                        <FormHelperText>Select grade level</FormHelperText>
+                    </FormControl>
+    
+                    <label > Upload CSV or EXCEL file
+                        <input type="file" onChange={handleFileChange} style={{ marginLeft: "1em"}}/>
+                    </label>
+                    
+                    <button onClick={handleUpload}>Upload</button>
+                </div>
+                
+            </div>
+        );
+    }
 }
 
 export default App;
-
-
-
-
-
-
-
-{/* <label>
-Select Test Type:
-<select value={testType} onChange={handleTestTypeChange}>
-    <option value="Fastbridge">Fastbridge</option>
-    <option value="DIBELS">DIBELS</option>
-    <option value="CBM">CBM</option>
-</select>
-</label>
-<label>
-Select Grade Level:
-<select value={gradeLevel} onChange={handleGradeLevelChange}>
-    <option value="K">K</option>
-    <option value="1">1</option>
-    <option value="2">2</option>
-    <option value="3">3</option>
-    <option value="4">4</option>
-    <option value="5">5</option>
-</select>
-</label>
-<br /><br /> */}
--- server/server.js
+++ server/server.js
@@ -4,33 +4,31 @@ import csvParser from "csv-parser";
 import xlsx from "xlsx";
 import fs from "fs";
 import cors from "cors";
+import { calculateMedian } from "./utils";
 
 const app = express();
 const PORT = 8000;
 
 // Update the CORS options to allow requests from frontend
 const corsOptions = {
-    origin: ["http://localhost:5173"], // Include the frontend URL
+    origin: ["http://localhost:5173", "localhost:5173"], // Include the frontend URL
   };
 app.use(cors(corsOptions));
 
 const upload = multer({ dest: "uploads/" });
 
-function calculateMedian(numbers) {
-    numbers.sort((a, b) => a - b);
-    const mid = Math.floor(numbers.length / 2);
-    return numbers.length % 2 !== 0
-        ? numbers[mid]
-        : (numbers[mid - 1] + numbers[mid]) / 2;
-}
 
 app.post("/upload", upload.single("file"), (req, res) => {
+    console.log("recieved upload request")
     const filePath = req.file.path;
     const extension = req.file.originalname.split(".").pop();
+    console.log("file path: ", filePath, "extension: ", extension)
 
     let scores = [];
 
     if (extension === "csv") {
+        console.log("inside csv process")
+        
         fs.createReadStream(filePath)
             .pipe(csvParser())
             .on("data", (row) => {
@@ -39,21 +37,39 @@ app.post("/upload", upload.single("file"), (req, res) => {
             })
             .on("end", () => {
                 fs.unlinkSync(filePath);
-                res.json({ median: calculateMedian(scores) });
+
+                console.log("scores: ", scores)
+                const median = calculateMedian(scores)
+                console.log("median: ", median)
+
+                res.json({ median: median, scores: scores });
             });
+
     } else if (extension === "xlsx") {
+        console.log("inside xlsx process")
         const workbook = xlsx.readFile(filePath);
+
         const sheetName = workbook.SheetNames[0];
+        console.log("sheet name: ", sheetName)
+
         const data = xlsx.utils.sheet_to_json(workbook.Sheets[sheetName]);
+        console.log("data: ", data)
 
         scores = data.map((row) => parseFloat(row["Score"])).filter((n) => !isNaN(n));
-
+    
         fs.unlinkSync(filePath);
-        res.json({ median: calculateMedian(scores) });
+
+        console.log("scores: ", scores)
+        const median = calculateMedian(scores)
+        console.log("median: ", median)
+
+        res.json({ median: median, scores: scores });
     } else {
         fs.unlinkSync(filePath);
         res.status(400).json({ error: "Invalid file format" });
     }
 });
+
+
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
